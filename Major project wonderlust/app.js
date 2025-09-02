0865e0e38953437c910202305a0c9c41cf2c9a6e
pk8789764353 <priyanshukumar5104@gmail.com>
2025-09-02 21:05:34 +0530

    fg

---
+++ Major project wonderlust/app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const Listing = require("./models/listing.js");
+const ejsMate=require("ejs-mate");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static("public"));
+
+
+
+main()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+}
+
+app.get("/", (req, res) => {
+    res.send("okay");
+})
+
+
+//Index route
+app.get("/listings", async (req, res) => {
+    const newListing = await Listing.find({});
+    res.render("listings/index.ejs", { newListing });
+})
+
+//new routte
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+//redirect route
+app.post("/listings", async (req, res) => {
+    let newListings = new Listing(req.body.listings);
+    await newListings.save();
+    res.redirect("/listings");
+
+})
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listings = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listings });
+})
+//update route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    Listing.findByIdAndUpdate(id, { ...req.body.listings });
+    res.redirect("/listings");
+})
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+
+})
+
+//new routte
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+//delte route
+app.delete("/listings/:id/delete", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
+
+app.listen(8080, () => {
+    console.log("okay");
+
+})
