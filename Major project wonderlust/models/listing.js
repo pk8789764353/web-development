2ecc1d19e03ecafe922997bf3aaae78e1dc4344f
pk8789764353 <priyanshukumar5104@gmail.com>
2025-09-19 20:55:27 +0530

    dsd

--- Major project wonderlust/models/listing.js
+++ Major project wonderlust/models/listing.js
@@ -18,7 +18,6 @@ const listingSchema = new Schema({
     default: "https://unsplash.com/photos/double-rainbow-over-desert-buttes-at-sunset-Ngj2u4PHjBY"
   }
 },
-
   price: Number,
   location: String,
   country: String,
